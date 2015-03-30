9fb666a16a669e1df4a987f167f97243504d75c3
Carlos del Cerro <carlosc7fuen@gmail.com>
2015-03-30 12:38:49 +0200

    Ecuación de segundo grado

---
+++ Java/src/pruebas/Ecuacion2Grado.java
@@ -0,0 +1,32 @@
+package pruebas;
+
+import java.util.Scanner;
+
+public class Ecuacion2Grado {
+
+	public static void main(String[] args) {
+		double a, b, c;
+		double x1, x2, d;
+		@SuppressWarnings("resource")
+		//Lectura de teclado
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Introduce a: ");
+		a = sc.nextInt();
+		System.out.println("Introduce b");
+		b = sc.nextInt();
+		System.out.println("Introduce c");
+		c = sc.nextInt();
+
+		d = ((b * b) - 4 * a * c);
+		if (d < 0)
+			System.out.println("No soluciones");
+		else {
+			x1 = (-b + Math.sqrt(d)) / (2 * a);
+			x2 = (-b - Math.sqrt(d)) / (2 * a);
+			System.out.println(+x1);
+			System.out.println(+x2);
+		}
+	}
+
+}
